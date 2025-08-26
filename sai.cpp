bb1a215a7c21a2bb94cf436aad76480d24859a6f
Sai <saikokkula@gmail.com>
2025-08-26 16:48:19 +0530

    new innit

---
+++ sai.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main(){
+    int a;
+    cout<<"ENter a numbeer"<<endl;
+    cin>>a;
+    for(int i=2;i<=sqrt(a);i++){
+        if (a%i==0){
+            cout<<"Number is not prime"<<endl;
+            return 0;
+        }    
+        
+}
+cout<<"Prime"<<endl;
+cout<<"babu"<<endl;
+
+return 0;
+}
